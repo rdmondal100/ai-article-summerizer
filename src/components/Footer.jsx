5d66064fbe4c8fed16f6f4ddc73806cf82412fb3
Riday <rdmondal100@gmail.com>
2024-07-27 23:08:25 +0600

    added the react icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,68 +4,67 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 import { ImLinkedin } from "react-icons/im";
 import { FaSquareInstagram } from "react-icons/fa6";
 
-
 const Footer = () => {
-  const socialLinks = [
-    {
-      name: "facebook",
-      icon:<FaSquareFacebook/>,
-      link:"#",
-      color:"text-blue-600"
-      
+	const socialLinks = [
+		{
+			name: "facebook",
+			icon: <FaSquareFacebook />,
+			link: "#",
+			color: "text-blue-600",
+		},
+		{
+			name: "instagram",
+			icon: <FaSquareInstagram />,
+			link: "#",
+			color: "text-red-600",
+		},
+		{
+			name: "twitter",
+			icon: <FaSquareXTwitter />,
+			link: "#",
+			color: "text-zinc-800",
+		},
+		{
+			name: "linkedin",
+			icon: <ImLinkedin />,
+			link: "#",
+			color: "text-blue-800",
+		},
+		{
+			name: "github",
+			icon: <FaGithub />,
+			link: "#",
+			color: "text-zinc-800",
+		},
+	];
+	return (
+		<footer className='  w-full  flex justify-around items-center  p-5  font-bold text-black flex-col gap-5'>
+			<div className=' socials  flex gap-10 transition-all '>
+				{socialLinks?.map((item) => (
+					<a
+						href={item?.link}
+						key={item?.name}
+						className={` transition-all duration-200 hover:scale-110  text-2xl ${item?.color}`}
+					>
+						{item?.icon}
+					</a>
+				))}
+			</div>
 
-    },
-    {
-      name: "instagram",
-      icon:<FaSquareInstagram/>,
-      link:"#",
-      color:"text-red-600"
+			<div className='copyright   text-black'>
+				© 2024{" "}
+				<a
+					href='http://riday.tech'
+					target='_blank'
+					rel='noopener noreferrer'
+					className=' own-gradien'
+				>
+					Riday
+				</a>
+				, all rights reserved
+			</div>
+		</footer>
+	);
+};
 
-    },
-    {
-      name: "twitter",
-      icon:<FaSquareXTwitter/>, 
-      link:"#",
-      color:"text-zinc-800"
-
-
-    },
-    {
-      name: "linkedin",
-      icon:<ImLinkedin/>,
-      link:"#",
-      color:"text-blue-800"
-
-
-    },
-    {
-      name: "github",
-      icon:<FaGithub/>,
-      link:"#",
-      color:"text-zinc-800"
-
-
-    },
-  ]
-  return (
-    <footer className="  w-full  flex justify-around items-center  p-5  font-bold text-black flex-col gap-5">
-
-        <div className=" socials  flex gap-10 transition-all ">
-            {
-              socialLinks?.map((item)=>(
-                <a href={item?.link} key={item?.name} className={` transition-all duration-200 hover:scale-110  text-2xl ${item?.color}`}>{item?.icon}</a>
-
-              ))
-            }
-
-        </div>
-
-        <div className="copyright   text-black">
-      © 2024 <a href="http://riday.tech" target="_blank" rel="noopener noreferrer" className=" own-gradien">Riday</a>, all rights reserved
-      </div>
-
-    </footer>
-  )
-}
-
-export default Footer
+export default Footer;
